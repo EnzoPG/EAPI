fdb3bf94a0890e82567701a980fb9448bcdf17a5
Enzo <enzo.gerola@fatec.sp.gov.br>
2019-06-04 15:58:15 -0300

    List all the products reviews on the same page
    localhost:8000/api/produtos

--- app/Http/Resources/Produto/ProdutoResource.php
+++ app/Http/Resources/Produto/ProdutoResource.php
@@ -22,10 +22,7 @@ class ProdutoResource extends JsonResource
       'prod_stoque' => $this->prod_stoque == 0 ? 'Sem Estoque' : $this->prod_stoque,
       'prod_desc' => $this->prod_desc . '%',
       'valor_final' => $this->prod_desc == 0 ? $this->prod_preco : round(($this->prod_preco - ($this->prod_desc/100) * $this->prod_preco), 2),
-      'avaliacoes' => Analises::find($this->prod_id)->count() > 0 ? round(Analises::find($this->prod_id)->sum('avals') / Analises::find($this->prod_id)->count(), 2) : 'Ainda não avaliado',
-      'href' => [
-        'analises' => route('analises.index', Analises::find($this->prod_id))
-      ]
+      'avaliacoes' => Analises::find($this->prod_id)->count() > 0 ? Analises::where('prod_id', $this->prod_id)->get() : 'Ainda não avaliado'
     ];
   }
 }
