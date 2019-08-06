dc921ed23c54c5e8e91c5fea758eb9811407c24b
Enzo <enzo.gerola@fatec.sp.gov.br>
2019-08-06 10:25:46 -0300

    Inserting Products

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Model\Produtos;
 use Illuminate\Http\Request;
+use App\Http\Requests\ProdutosRequest;
 use App\Http\Resources\Produto\ProdutoResource;
 use App\Http\Resources\Produto\ProdutoCollection;
 
 class ProdutosController extends Controller
 {
+
+  public function __construct(){
+      $this->middleware('auth:api')->except('index', 'show');
+  }
+
   /**
   * Display a listing of the resource.
   *
@@ -35,9 +41,18 @@ class ProdutosController extends Controller
   * @param  \Illuminate\Http\Request  $request
   * @return \Illuminate\Http\Response
   */
-  public function store(Request $request)
+  public function store(ProdutosRequest $request)
   {
-    //
+    $Produto = new Produto;
+    $Produto->prod_nome = $request->prod_nome;
+    $Produto->prod_details = $request->prod_details;
+    $Produto->prod_preco = $request->prod_preco;
+    $Produto->prod_desc = $request->prod_desc;
+    $Produto->save();
+
+    return response ([
+      'data' => ProdutoResource($Produto);
+    ],Response::HTTP_CREATED);
   }
 
   /**
